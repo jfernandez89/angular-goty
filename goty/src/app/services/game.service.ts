1532cee55f2905b39ca8a9dca69193b0b10a49af
jfernandez89 <javier.fernandez@s-dos.es>
2020-08-28 12:38:39 +0200

    added game structure, also some improvements were applied in the service

--- goty/src/app/services/game.service.ts
+++ goty/src/app/services/game.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { GameDTO } from '../models/GameDTO';
 import { ApiService } from './api.service';
@@ -10,6 +10,7 @@ import { ApiService } from './api.service';
 })
 export class GameService extends ApiService {
   private readonly API_PATH = '/api/goty';
+  private games: GameDTO[] = [];
 
   constructor(protected http: HttpClient) {
     super(http);
@@ -17,13 +18,20 @@ export class GameService extends ApiService {
 
   // GET GOTY //
   public getGoty(): Observable<GameDTO[]> {
-    return this.http.get(`${this.baseUrl}${this.API_PATH}`).pipe(
-      map((response) => {
-        return response as GameDTO[];
-      }),
-      catchError((error) => {
-        return throwError(error);
-      })
-    );
+    // Controls if we had recover the data once at least
+    if (this.games.length > 0) {
+      // Return the games as an observable
+      return of(this.games);
+    } else {
+      return this.http.get(`${this.baseUrl}${this.API_PATH}`).pipe(
+        map((response: GameDTO[]) => {
+          this.games = response;
+          return this.games;
+        }),
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+    }
   }
 }
